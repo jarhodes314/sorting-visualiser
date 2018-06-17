2bc1bbe4fa99cd543ddf9c1f5bdfe56843b4cd61
jarhodes314 <jarhodes314@gmail.com>
2018-06-17 23:44:12 +0100

    Began to add support for custom lists that cache stuff

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ A sorting algorithm visualiser written in C#. It is based on a Java project (lar
 This project will use [Eto.Forms](https://github.com/picoe/Eto) for first-class, cross-platform support for a GUI.
 
 ## What has been done
-* Nothing as of yet :cry:, but I only started on 15th June 2018, so I have an excuse :smile:
+* Create basic interface using Eto
 
 ## Things to do
-* Create basic interface using Eto
+* Create method to draw the visualisations
 * Create support for calculating disparity in a list, either using caching or on-the-fly
 * Add some algorithms
 
---
+++ sorting-visualiser/DisparityCachedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace sorting_visualiser
+{
+    public class DisparityCachedList
+    {
+        /* a class to calculate and store the list at every step
+         * of the visualisation
+         * 
+         * the use of the two different lists will make the algorithm
+         * easy to implement and the visualisation easy to run
+         * 
+         * a DisparityList will be converted to a DisparityCachedList
+         * once the algorithm has terminated
+         */
+
+        public struct DisparityValuePair
+        {
+            int Disparity, Value;
+
+            private DisparityValuePair(int d, int v){
+                Disparity = d;
+                Value = v;
+            }
+        }
+
+        public DisparityCachedList()
+        {
+        }
+    }
+}
