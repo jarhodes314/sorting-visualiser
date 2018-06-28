eaf89b73face2d05f8892991e1991a0d78712755
jarhodes314 <jarhodes314@gmail.com>
2018-06-28 15:17:03 +0100

    Hopefully neater now

---
+++ RhodesSort.Visualiser/Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace sorting_visualiser
+{
+    public struct PositionXY
+    {
+        int x;
+        int y;
+
+        public PositionXY(int xval, int yval)
+        {
+            x = xval;
+            y = yval;
+        }
+    }
+
+    public struct PositionRΘ
+    {
+        double r;
+        double t;
+
+        public PositionRΘ(double rval, double theta)
+        {
+            r = rval;
+            t = theta;
+        }
+    }
+}
